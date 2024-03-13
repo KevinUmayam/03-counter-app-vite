9ee394025e03f0067bb1e22c4506841d30082d1b
Kevin Umayam <kevin.m.umayam@gmail.com>
2024-03-12 20:08:46 -0700

    added styles and applied them by importing them

--- src/FirstApp.jsx
+++ src/FirstApp.jsx
@@ -1,6 +1,11 @@
 const FirstApp = () => {
   return (
-    <div>FirstApp</div>
+    <>
+      <h1>Kevin Umayam</h1>
+      <p>this is a subtitle </p>
+    </>
+    
+
   )
 }
 export default FirstApp
